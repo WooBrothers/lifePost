15393bd0566ea902f9b47879e0117a26b85f43a4
wookim789 <wookim789@gmail.com>
2023-06-12 09:57:10 +0900

    develop config 파일 이동

--- src/main/java/com/woobros/member/hub/config/AppConfig.java
+++ src/main/java/com/woobros/member/hub/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.woobros.member.hub.bean;
+package com.woobros.member.hub.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
