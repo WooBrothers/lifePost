01fe77b20c2bbb72fc0b3b0c8ef7556297134e55
wookim789 <wookim789@gmail.com>
2023-08-31 19:25:53 +0900

    develop cd 무중단 배포 테스트8

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ echo "deploy start."
 
 cd /home/ec2-user/myapp/lifepost/service
 
-docker build -t lifepost-service .
+docker build -t lifepost-service -f lifepost-service.Dockerfile .
 
 docker compose up -d
