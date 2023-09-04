5cff5c62e2f97631c83bff82c873eaf717b270b4
wookim789 <wookim789@gmail.com>
2023-09-04 09:59:58 +0900

    develop 해방7

--- log.sh
+++ log.sh
@@ -1,3 +1,11 @@
-exec > /output.txt 2>&1
+# 로그 파일 경로
+log_file="./myapp.log"
 
-echo "codedeploy run this file."
+# 로그 메시지
+log_message="This is a log message."
+
+# 로그를 파일에 추가 (권한 문제를 방지하기 위해 sudo 사용)
+sudo bash -c "echo $(date) - $log_message >> $log_file"
+
+# 스크립트 실행 로그 출력
+echo "Logged: $log_message"
