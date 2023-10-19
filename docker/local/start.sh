d7205a312a5ce9603dd3b6f501cd88241cf6562b
wookim789 <wookim789@gmail.com>
2023-10-19 16:45:03 +0900

    develop 민감정보 분리 확인

--- docker/local/start.sh
+++ docker/local/start.sh
@@ -1,7 +1,5 @@
 echo "deploy start."
 
-cd /home/ec2-user/myapp/lifepost/service
-
 docker build -t lifepost-service -f lifepost-service.Dockerfile .
 
-docker-compose --env-file ../env/.env up -d
+docker-compose --env-file ../env/.env.local up -d
