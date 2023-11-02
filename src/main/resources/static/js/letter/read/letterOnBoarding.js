580339a79101b111da1733fbc98aecc775ad424f
wookim789 <wookim789@gmail.com>
2023-11-02 15:57:30 +0900

    develop 편지 온보딩 고도화
    - 마지막 페이지 도달 시 다음 버튼 닫기 버튼으로 대체
    - 닫힌 모달은 처음부터 출력되도록 수정

--- src/main/resources/static/js/letter/read/letterOnBoarding.js
+++ src/main/resources/static/js/letter/read/letterOnBoarding.js
@@ -8,6 +8,7 @@ function bindEventToOnBoardingModal() {
     nextBtn.addEventListener("click", nextBtnClick);
 }
 
+/* event */
 function nextBtnClick() {
     let pageId = parseInt(document.querySelector(".dot.active").dataset.dotId);
     const maxPageId = document.querySelectorAll(".on-boarding").length;
@@ -15,19 +16,18 @@ function nextBtnClick() {
     if (pageId < maxPageId) {
         handleDot(pageId, pageId + 1);
         handleModalPage(pageId, pageId + 1);
-        handlePreviousBtn();
+        ablePreviousBtn();
 
         pageId++;
     }
 
     if (pageId === maxPageId) {
-        this.classList.add("disabled");
-    }
-}
+        this.classList.add("end-btn");
+        this.innerHTML = "시작하기";
 
-function handlePreviousBtn() {
-    const previousBtn = document.querySelector("#previous-btn");
-    previousBtn.classList.remove("disabled");
+        const endBtn = document.querySelector(".end-btn");
+        endBtn.addEventListener("click", endBtnClick);
+    }
 }
 
 function previousClick() {
@@ -37,20 +37,55 @@ function previousClick() {
     if (pageId > minPageId) {
         handleDot(pageId, pageId - 1);
         handleModalPage(pageId, pageId - 1);
-        handleNextBtn();
+        ableNextBtn();
+        rollbackEndBtn();
 
         pageId--;
     }
     if (pageId === minPageId) {
-        this.classList.add("disabled");
+        disablePreviousBtn();
     }
 }
 
-function handleNextBtn() {
+function endBtnClick() {
+    this.innerHTML = "다음";
+    this.removeEventListener("click", endBtnClick);
+
+    handleDot(6, 1);
+    handleModalPage(6, 1);
+    ableNextBtn();
+    disablePreviousBtn();
+
+    const modalCloseBtn = document.querySelector("#modal-close-btn");
+    modalCloseBtn.click();
+}
+
+
+/* function */
+function rollbackEndBtn() {
+    const endBtn = document.querySelector(".end-btn");
+
+    if (endBtn) {
+        endBtn.classList.remove("end-btn");
+        endBtn.removeEventListener("click", endBtnClick);
+        endBtn.innerHTML = "다음";
+    }
+}
+
+function ablePreviousBtn() {
+    const previousBtn = document.querySelector("#previous-btn");
+    previousBtn.classList.remove("disabled");
+}
+
+function disablePreviousBtn() {
+    const previousBtn = document.querySelector("#previous-btn");
+    previousBtn.classList.add("disabled");
+}
+
+function ableNextBtn() {
     const nextBtn = document.querySelector("#next-btn");
     nextBtn.classList.remove("disabled");
 }
-
 
 function handleDot(pageId, resultId) {
     const activeDot = document.querySelector(`#dot-${pageId}`);
